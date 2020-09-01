03bb44c1ce23dbd7723ef01cfad7281448bf60c2
William Graf <7930703+willgraf@users.noreply.github.com>
2020-09-01 16:20:47 -0700

    update tests for better coverage

--- src/test/java/org/vanvalenlab/LabelROIConverterTest.java
+++ src/test/java/org/vanvalenlab/LabelROIConverterTest.java
@@ -18,30 +18,38 @@ import org.junit.rules.TemporaryFolder;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class LabelROIConverterTest {
     private BufferedImage img;
     private ImagePlus imp;
 
-    @Before
-    public void setUp() throws IOException {
-        img = new BufferedImage(20, 20, BufferedImage.TYPE_INT_ARGB);
-        img.setRGB(0, 1, 1); // set an "object"
-        img.setRGB(0, 2, 1); // set an "object"
-        imp = new ImagePlus("test.tiff", img);
+    public ImagePlus getTestImage(int width) {
+        BufferedImage img = new BufferedImage(width, width, BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < width; i++) {
+            if (i > width / 2) {
+                img.setRGB(i, i, 1); // set an "object"
+            }
+        }
+        ImagePlus imp = new ImagePlus("test.tiff", img);
+        return imp;
     }
 
     @Test
     public void testWandToRoi() {
-        Wand wand = new Wand(imp.getProcessor());
-        wand.autoOutline(0, 0, 1.0, Wand.EIGHT_CONNECTED);
-        PolygonRoi roi = LabelROIConverter.wandToRoi(wand);
-        assert (roi.containsPoint(0, 1));
-        assert (roi.containsPoint(0, 2));
+        int[] widths = {20, 2000};
+        for (int width : widths) {
+            ImagePlus imp = getTestImage(width);
+            Wand wand = new Wand(imp.getProcessor());
+            wand.autoOutline(0, 0, 1.0, Wand.EIGHT_CONNECTED);
+            PolygonRoi roi = LabelROIConverter.wandToRoi(wand);
+            assert (roi.containsPoint(width / 2 + 1, width / 2 + 1));
+        }
     }
 
     @Test
     public void testCreateLabelOverlay() {
+        ImagePlus imp = getTestImage(20);
         LabelROIConverter.createLabelOverlay(imp);
         assert (imp.getOverlay() != null);
     }
